d3dca3d8030e429901b35b919dc874a83c3b8e43
Altaf Quadri <altafmquadri@gmail.com>
2021-03-01 16:51:44 -0500

    add components search bar and page list, add service wikipedia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wsearch';
+  constructor(private wikipedia: WikipediaService) {}
+  onTerm(term: string) {
+    const results = this.wikipedia.search(term);
+    console.log(results);
+  }
 }
